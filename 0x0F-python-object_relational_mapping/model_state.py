be6a8c3e6903c7121fbe860199a2f1c881b47526
Agabaofficial <agabaolivier85@gmail.com>
2023-09-20 17:25:10 -0700

    corrected model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""python file that contains the class definition of a State and an instance Base = declarative_base():
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
